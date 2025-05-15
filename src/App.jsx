4f735490be185eee2dbda516093172c9c61101ac
Diego <bastosdiegol@gmail.com>
2025-05-15 17:41:31 -0400

    chore: add default react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      Portfolio
+    </>
+  );
+}
+
+export default App;
